00e1cb0a2028ff21502549a4b101040863083ae7
universoglauka <universoglauka@gmail.com>
2026-08-03 02:06:21 -0300

    Update navproductor.blade.php

--- resources/views/layouts/navproductor.blade.php
+++ resources/views/layouts/navproductor.blade.php
@@ -111,7 +111,7 @@
       </div>
 
       <div class="mt-3 space-y-1">
-        <x-responsive-nav-link :href="route('profile.edit')">
+        <x-responsive-nav-link :href="route('profile')">
           {{ __('Profile') }}
         </x-responsive-nav-link>
 
