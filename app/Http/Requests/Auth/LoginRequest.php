9c31f056c7cbce69d0eeabce771ad9109d1f430c
universoglauka <universoglauka@gmail.com>
2026-08-03 20:17:45 -0300

    Mensaje de error de Login en español

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -45,7 +45,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
+                'email' => 'El correo electrónico o la contraseña son incorrectos.',
             ]);
         }
 
